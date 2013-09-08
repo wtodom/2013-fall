cd744955b5c5d1464a8e1feec2795dd1ad94aa57
Weston Odom <weston.odom@gmail.com>
2013-09-08 17:26:24 -0500

    MYSTERYYYYYYYYYY

--- cs403/mystery.scm
+++ cs403/mystery.scm
@@ -1,11 +1,20 @@
 (define (firstDigitAt depth)
-	(+ 1 (* 4 (- depth 1)))
+	(cond
+		((= depth 0) 1)
+		(else
+			(+ 1 (* 4 depth))
+			)
+		)
 	)
 
-(define (mystery depth)
-
-	)
-
+; (define (mystery depth)
+; 	(cond
+; 		((= depth 0) 1)
+; 		(else
+; 			(+ (/ 1 (+ (firstDigitAt depth) .5)) (mystery (- depth 1)))
+; 			)
+; 		)
+; 	)
 
 (define (iMystery depth)
 	(define (innerMystery i total)
@@ -19,9 +28,23 @@
 	(innerMystery depth 0)
 	)
 
-(inspect (iMystery 0))
-(inspect (iMystery 1))
-(inspect (iMystery 2))
-(inspect (iMystery 3))
-(inspect (iMystery 4))
-(inspect (iMystery 5))
+(define (square x)
+	(* x x)
+	)
+
+(define (mystery depth)
+	(define (innerMystery i)
+		(cond
+			((>= i depth) 1)
+			(else ; not quite the right recursion, or maybe not right recombination
+				(+ 1 (/ 1.0 (+ (firstDigitAt i) (/ 1.0 (+ 1 (/ 1.0 (innerMystery (+ i 1))))))))
+				)
+			)
+		)
+	(innerMystery 0)
+	)
+
+(inspect (mystery 0))
+(inspect (mystery 1))
+(inspect (mystery 2))
+(inspect (mystery 175))

9b3a1ec93a198c8e462941a1657e94c02a7a670c
Weston Odom <weston.odom@gmail.com>
2013-09-21 14:45:01 -0500

    randomness implemented in diff, almost thereeeee

--- cs403/assign2/diff.scm
+++ cs403/assign2/diff.scm
@@ -1,4 +1,16 @@
-(define (index element collection)
+(include "random.lib")
+(randomSeed (int (time)))
+(define WORD_COUNT (- 210680 1))
+
+(define (abs x) ;;; DONE ;;;
+	(if (< x 0)
+		(- x)
+	;else
+		x
+		)
+	)
+
+(define (index element collection) ;;; DONE ;;;
 	(define (indexIter count tail)
 		(cond
 			((null? tail) -1)
@@ -13,19 +25,19 @@
 
 ; (inspect (index 5 (list 1 2 3 4 5 6 7)))
 
-(define (letterValue letter)
+(define (ascii letter) ;;; DONE ;;;
 	(define letters "abcdefghijklmnopqrstuvwxyz")
 	(+ (index letter letters) 1)
 	)
 
-; (inspect (letterValue "b"))
+; (inspect (ascii "b"))
 
-(define (word2int word)
+(define (word2int word) ;;; DONE ;;;
 	(define (converterator restOfWord sum)
 		(cond
 			((null? restOfWord) sum)
 			(else
-				(converterator (cdr restOfWord) (+ sum (letterValue (car restOfWord))))
+				(converterator (cdr restOfWord) (+ sum (ascii (car restOfWord))))
 				)
 			)
 		)
@@ -33,3 +45,59 @@
 	)
 
 ; (inspect (word2int "ab"))
+
+(define (diff word1 word2) ;;; DONE ;;;
+	(define p (open "words" 'read))   ; p points to a port
+    (define oldInput (setPort p))
+	
+	(define value (abs (- (word2int word1) (word2int word2))))
+
+	(setPort oldInput)
+	(close p)
+
+	value
+	)
+
+; (inspect (diff "love" "hate"))
+
+(define (kthWord k) ;;; DONE ;;;
+	(define p (open "words" 'read))   ; p points to a port
+    (define oldInput (setPort p))
+
+    (define (readIter word i)
+    	(cond
+    		((= i k) word)
+    		(else
+    			(readIter (readLine) (+ i 1))
+    			)
+    		)
+    	)
+    (define w (readIter (readLine) 0))
+
+	(setPort oldInput)
+	(close p)
+	w
+	)
+
+; (inspect (kthWord (randomRange 0 WORD_COUNT)))
+
+(define (randomWord)
+	(kthWord (randomRange 0 WORD_COUNT))
+	)
+
+; (inspect (randomWord))
+
+(inspect (diff (randomWord) (randomWord)))
+
+;;; TODO ;;;
+(define (getMatch value)
+	(define p (open "words" 'read))   ; p points to a port
+    (define oldInput (setPort p))
+	
+    ; find the word
+
+	(setPort oldInput)
+	(close p)
+
+	; return the word
+	)

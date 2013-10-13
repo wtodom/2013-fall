48979028639b1c341b3fa0485c6bab84fb55c9bc
Weston Odom <weston.odom@gmail.com>
2013-10-12 22:53:46 -0500

    minor progress for assignment 2 # 10

--- cs403/assign2/objects.scm
+++ cs403/assign2/objects.scm
@@ -1,11 +1,11 @@
 (define oldplus +)
 
-(define (Integer i)
-	(define (add intA intB) ; TODO
-		(oldplus intA intB)
+(define (Integer val)
+	(define (add other) ; TODO
+		(Integer (oldplus ((value)) ((other'value))))
 		)
 	(define (promote) ; DONE
-		(Rational i 1)
+		(Rational val 1)
 		)
 	(define (demote) ; DONE
 		this
@@ -14,85 +14,110 @@
 		0
 		)
 	(define (toString) ; DONE
-		(string i)
+		(string val)
 		)
 	(define (value) ; DONE
-		i
+		val
 		)
 	this
 	)
 
-(inspect (((Integer 0)'toString)))
-(println "    [it should be 0]\n")
-(inspect (((Integer 0)'rank)))
-(println "    [it should be a number]\n")
-(inspect (((+ (Integer 0) (Integer 0) (Integer 0))'value)))
-(println "    [it should be 0]\n")
-(inspect (+ 3 5 1))
-(println "    [it should be 9]\n")
+(define (gcd a b) ; taken from rosettacode.org
+	(if (= b 0)
+		a
+		(gcd b (% a b))
+		)
+	)
 
 (define (Rational numerator denominator)
-	(define (add ratA ratB) ; TODO
+	(define (simplest n-or-d) ; pass 'n or 'd to get that term in lowest form
+		(cond
+			((eq? n-or-d 'n) (/ numerator (gcd numerator denominator)))
+			(else
+				(/ denominator (gcd numerator denominator))
+				)
+			)
+		)
+	(define (add other) ; TODO
 
 		)
-	(define (promote) ; TODO
-
+	(define (promote) ; DONE
+		(Real (* (/ (simplest 'n) (simplest 'd)) 1000000) 6)
 		)
-	(define (demote) 
-		(Integer (int (/ numerator denominator)))
+	(define (demote) ; DONE
+		(Integer (int (/ (simplest 'n) (simplest 'd))))
 		)
 	(define (rank) ; DONE
-		1
+		1 
 		)
 	(define (toString) ; DONE
-		(string+ (string numerator) "/" (string denominator))
+		(string+ (string (simplest 'n)) "/" (string (simplest 'd)))
 		)
-	(define (value) ; TODO
-
+	(define (value) ; DONE
+		(/ (simplest 'n) (simplest 'd))
 		)
 	this
 	)
 
-(define (Real integerPart decimalPart)
-	(define (add realA realB) ; TODO
+; (inspect (((Rational 3 5)'toString)))
+
+(define (Real integerVersion decIndex)
+	(define (add other) ; TODO
 
 		)
-	(define (promote) ; TODO
-
+	(define (promote) ; DONE
+		(Complex (value) 0)
 		)
 	(define (demote) ; TODO
-
+		(Rational )
 		)
 	(define (rank) ; DONE
 		2
 		)
 	(define (toString) ; DONE
-		(string+ (string integerPart) "." (string decimalPart))
+		(string+ (string integerVersion) "e-" (string decIndex))
 		)
-	(define (value) ; TODO
-
+	(define (value) ; DONE
+		(/ (real integerVersion) (^ 10 decIndex))
 		)
 	this
 	)
 
+(inspect (((Real 3 5)'toString)))
+(inspect (((Real 3 5)'value)))
+
 (define (Complex realPart imaginaryPart)
-	(define (add complexA complexB) ; TODO
+	(define (add other) ; TODO
 
 		)
 	(define (promote) ; DONE
 		this
 		)
-	(define (demote) ; TODO
-
+	(define (demote) ; DONE
+		(cond
+			((not (contains (string realPart) ".")) (Real realPart (length (string realPart))))
+			(else
+				(define decIndex (indexOf "." (string realPart)))
+				(Real 
+					(int 
+						(string+ 
+							(substring 0 decIndex)
+							(substring (+ decIndex 1) (length (string realPart)))
+							)
+						)
+					decIndex
+					)
+				)
+			)
 		)
 	(define (rank) ; DONE
 		3
 		)
-	(define (toString) ; TODO
-
+	(define (toString) ; DONE
+		(string+ realPart "+" imaginaryPart "i")
 		)
-	(define (value) ; TODO
-
+	(define (value) ; DONE
+		(list realPart imaginaryPart)
 		)
 	this
 	)

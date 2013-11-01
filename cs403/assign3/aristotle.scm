d20aeafd9c062f57540723f22dc5a1dcafa96fa0
Weston Odom <weston.odom@gmail.com>
2013-10-31 19:48:26 -0500

    Aristotle (assignment 3, #8) kinda done. seems correct according to guidelines, but causes error.

---
+++ cs403/assign3/aristotle.scm
@@ -0,0 +1,56 @@
+(include "stream.lib")
+
+(define fours (cons-stream 4 fours))
+
+(define powers-of-four
+	(cons-stream 1 (stream-mult fours powers-of-four)))
+
+(define numers ones)
+
+(define denoms (stream-cdr powers-of-four))
+
+; (inspect (stream-car powers-of-four))
+; (inspect (stream-car (stream-cdr powers-of-four)))
+; (inspect (stream-car (stream-cdr (stream-cdr powers-of-four))))
+; (inspect (stream-car (stream-cdr (stream-cdr (stream-cdr powers-of-four)))))
+
+(define ari (stream-div numers denoms))
+
+; (inspect (stream-car ari))
+; (inspect (stream-car (stream-cdr ari)))
+; (inspect (stream-car (stream-cdr (stream-cdr ari))))
+; (inspect (stream-car (stream-cdr (stream-cdr (stream-cdr ari)))))
+
+(define ari-ps (psum ari))
+
+; (inspect (stream-car ari-ps))
+; (inspect (stream-car (stream-cdr ari-ps)))
+; (inspect (stream-car (stream-cdr (stream-cdr ari-ps))))
+; (inspect (stream-car (stream-cdr (stream-cdr (stream-cdr ari-ps)))))
+
+(define ari-acc
+	(cons-stream
+		(stream-car ari-ps)
+		(stream-transform euler-transform ari-ps)
+		)
+	)
+
+(inspect (stream-car ari-acc))
+(inspect (stream-car (stream-cdr ari-acc)))
+(inspect (stream-car (stream-cdr (stream-cdr ari-acc))))
+(inspect (stream-car (stream-cdr (stream-cdr (stream-cdr ari-acc)))))
+
+(define ari-super
+	(stream-map
+		stream-car
+		(make-tableau-with-transform
+			ari-acc
+			euler-transform
+			)
+		)
+	)
+
+(inspect (stream-car ari-super))
+(inspect (stream-car (stream-cdr ari-super)))
+(inspect (stream-car (stream-cdr (stream-cdr ari-super))))
+(inspect (stream-car (stream-cdr (stream-cdr (stream-cdr ari-super)))))

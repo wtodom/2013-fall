6c35039f00fa97822a0518d8b53577288db1abb8
Weston Odom <weston.odom@gmail.com>
2013-11-10 20:27:52 -0600

    Some work on environments. May end up having to change this is they need to be implemented as Lexemes.

--- cs403/final-project/code/environment.py
+++ cs403/final-project/code/environment.py
@@ -1,24 +1,48 @@
+from collections import deque
 from lexeme import Lexeme
 
 class Environment:
 
-	def __init__(self):
-		self.envList = self.create()
+	def __init__(self, parent_env=None):
+		self.parent_env = parent_env
+		self.env_list = deque()
+		self.env_list.appendleft(self.create())
 
 	def create(self):
-		# maybe one of these:
-			# [[[],[]]]
-			# Lexeme(token_type="ENVIRONMENT", right=Lexeme(token_type="ENVIRONMENT"))
+		new_env = deque()
+		new_env.append(deque())
+		new_env.append(deque())
+		return new_env
+
+	def lookup(self, variable):
+		for env in self.env_list:
+			for i, var in enumerate(env[0]):
+				if var == variable:
+					return env[1][i]
+		return None
+
+	def update(self, variable, new_value):
+		for env in self.env_list:
+			for i, var in enumerate(env[0]):
+				if var == variable:
+					env[1][i] = new_value
+
+	def insert(self, variable, value):
+		self.env_list[0][0].appendleft(variable)
+		self.env_list[0][1].appendleft(value)
+
+	def extend(self, variables, values, parent):
+		# new_env = deque()
+		# new_env.append(deque(variables))
+		# new_env.append(deque(values))
+		# print(new_env)
 		pass
 
-	def lookup(self, variable, env_list):
-		pass
-
-	def update(self, variable, new_value, env_list):
-		pass
-
-	def insert(self, variable, value, env_list):
-		pass
-
-	def extend(self, variables, values, env_list):
-		pass
+e = Environment()
+print("Inserting variable x with value 5...")
+e.insert("x", 5)
+print("Attempting to lookup the variable 'x'...")
+print("x = " + str(e.lookup("x")))
+# print(e.env_list)
+# e.extend(["x", "y", "z", "w"], [1, 2, 3, 4], None)
+# print(e.env_list)

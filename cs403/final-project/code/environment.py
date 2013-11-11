8c19d7bbe1ff6452bc1b2693834f265f4db18d2d
Weston Odom <weston.odom@gmail.com>
2013-11-10 23:40:43 -0600

    Fixed makefile and started converting Environment to use Lexemes rather than a deque.

--- cs403/final-project/code/environment.py
+++ cs403/final-project/code/environment.py
@@ -1,35 +1,57 @@
-from collections import deque
 from lexeme import Lexeme
+from exceptions import UndefinedException
 
 class Environment:
 
 	def __init__(self, parent_env=None):
 		self.parent_env = parent_env
-		self.env_list = deque()
-		self.env_list.appendleft(self.create())
+		self.env_list = self.create()
 
 	def create(self):
-		new_env = deque()
-		new_env.append(deque())
-		new_env.append(deque())
-		return new_env
+		return Lexeme(token_type="ENVIRONMENT", right=Lexeme(tokenType="ENVIRONMENT"))
 
-	def lookup(self, variable):
-		for env in self.env_list:
-			for i, var in enumerate(env[0]):
-				if var == variable:
-					return env[1][i]
-		return None
+	def lookup(self, variable, env_list):
+		while env_list is not None:
+			vars = env_list.left
+			vals = env_list.right.left
+			while vars is not None:
+				if (sameVariable(variable, vars.left)):  # TODO: finish insert so I can figure this part out.
+					return vals.left
+				vars = vars.right
+				vals = vals.right
+			env_list = env_list.right.right
+
+		raise UndefinedException(variable)
+
+		# return None  # is this needed?
 
 	def update(self, variable, new_value):
-		for env in self.env_list:
-			for i, var in enumerate(env[0]):
-				if var == variable:
-					env[1][i] = new_value
+		# for env in self.env_list:
+		# 	for i, var in enumerate(env[0]):
+		# 		if var == variable:
+		# 			env[1][i] = new_value
+		pass
 
 	def insert(self, variable, value):
-		self.env_list[0][0].appendleft(variable)
-		self.env_list[0][1].appendleft(value)
+		self.env_list = Lexeme(token_type="JOIN", left=variable, right=self.env_list)
+		val = Lexeme(token_type="JOIN", left=value, right=self.env_list.right.left)
+        # setCar(
+        # 	envList,
+        # 	cons(
+        # 		JOIN,
+        # 		variable,
+        # 		car(envList)
+        # 		)
+        # 	)
+        # setCar(
+        # 	cdr(envList),
+        # 	cons(
+        # 		JOIN,
+        # 		value,
+        # 		cadr(envList)
+        # 		)
+        # 	)
+		pass
 
 	def extend(self, variables, values, parent):
 		# new_env = deque()
